4cb32507729f959534c3b01597f29c1bbeff4d3e
visparmar <pandoemp0158@pandoconsultants.com>
2026-08-02 14:35:49 +0530

    add terminal and extension section
    debug

--- _portfolio2.o/src/Layout.jsx
+++ _portfolio2.o/src/Layout.jsx
@@ -2,17 +2,19 @@ import { Outlet, Link } from 'react-router-dom';
 import FileDetails from "./component/FileDetails"
 import { RxCross2 } from "react-icons/rx";
 import FileContext from "./context/FileContext"
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { FaReact } from 'react-icons/fa';
+import { Terminal } from './Terminal';
 
 
 function Layout() {
-    const {openFile,setOpenFile} = useContext(FileContext)
+    const { openFile, setOpenFile } = useContext(FileContext)
+    const [terminalHeight, setTerminalHeight] = useState(180)
     console.log(openFile)
-    const deleteFileFromTopBar = (key) =>{
+    const deleteFileFromTopBar = (key) => {
         const updatedPerson = { ...openFile }; // Create a shallow copy
         delete updatedPerson[key]; // Remove the specified key
-        setOpenFile(updatedPerson); 
+        setOpenFile(updatedPerson);
     }
     return (
         <div className='flex w-screen h-screen'>
@@ -20,23 +22,28 @@ function Layout() {
                 <FileDetails />
             </div>
             {/* The child routes will be rendered here */}
-            <div className=' w-full h-full'>
-                <div className='h-full overflow-hidden hover:overflow-y-auto custom-scrollbar '>
-                <div className='w-full flex fixed bg-[#181818]'>
-                    {Object.keys(openFile).length > 0 &&  Object.keys(openFile).map((key)=>{
-                        return(
-                            <span key={key} className='border-t-2  w-max bg-[#181818] border border-[#2B2B2B] px-4 py-2 flex items-center gap-5 group'> 
-                            <FaReact  className='text-blue-400'/>
-                        <Link to={key}>{openFile[key]}</Link>
-                         <RxCross2 className='invisible group-hover:visible' onClick={()=>deleteFileFromTopBar(key)}/>
-                    </span>
-                        )
-                    })
-                    }
+            <div className='w-full h-full overflow-hidden'>
+
+                <div style={{
+                    height: `calc(100vh - ${terminalHeight}px)`
+                }} className={`overflow-hidden hover:overflow-y-auto custom-scrollbar `}>
+                    <div className='w-full flex fixed bg-[#181818]'>
+                        {Object.keys(openFile).length > 0 && Object.keys(openFile).map((key) => {
+                            return (
+                                <span key={key} className='border-t-2  w-max bg-[#181818] border border-[#2B2B2B] px-4 py-2 flex items-center gap-5 group'>
+                                    <FaReact className='text-blue-400' />
+                                    <Link to={key}>{openFile[key]}</Link>
+                                    <RxCross2 className='invisible group-hover:visible' onClick={() => deleteFileFromTopBar(key)} />
+                                </span>
+                            )
+                        })
+                        }
+                    </div>
+                    <Outlet />
                 </div>
-                <Outlet />
-                </div>                
+                <Terminal terminalHeight={terminalHeight} setTerminalHeight={setTerminalHeight} />
             </div>
+
         </div>
     );
 }
